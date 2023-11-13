d20981c21ebeea70c0e06aeb11ca782913907cca
Maheshvema <babumahesh2088@gmail.com>
2023-11-13 16:16:44 +0530

    modified

---
+++ Functions/OurClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    internal class OurClass
+    {
+        public void display(int workingHours=8,int workingdays=8,int projecthandle=0,int extra=0)
+        {
+            int result = workingHours * workingdays * 100 + projecthandle * 3000 + extra * 2000;
+            Console.WriteLine("Month Salary of Employee:\t"+result);
+        }
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OurClass hr= new OurClass();
+            hr.display(workingHours: 8, workingdays: 30);
+            OurClass Admin=new OurClass();
+            Admin.display(workingHours:8,workingdays:30,projecthandle:3);
+            OurClass softwaredeveloper=new OurClass();
+            softwaredeveloper.display(workingHours: 8, workingdays: 30, projecthandle: 4, extra: 5);
+            Console.ReadKey();
+        }
+    }
+}
